ea33a908ce3dc8a0bf7f18d947903b2c2d9bb020
Mike Johnston <mrbean355@gmail.com>
2019-07-03 21:49:00 +0200

    Improve media player usage
    Keep a list of players to allow multiple sounds to play at once.

--- src/main/kotlin/com/github/mrbean355/dota2/integration/assets/SoundBytePlayer.kt
+++ src/main/kotlin/com/github/mrbean355/dota2/integration/assets/SoundBytePlayer.kt
@@ -2,12 +2,12 @@ package com.github.mrbean355.dota2.integration.assets
 
 import javafx.scene.media.Media
 import javafx.scene.media.MediaPlayer
+import java.util.concurrent.CopyOnWriteArrayList
 import java.util.concurrent.atomic.AtomicBoolean
 
 private const val VOLUME = 0.20
-/* Prevent garbage collection. */
-private var mediaPlayer: MediaPlayer? = null
 private val initialised = AtomicBoolean(false)
+private val players = CopyOnWriteArrayList<MediaPlayer>()
 
 interface FileName {
     val fileName: String
@@ -18,9 +18,20 @@ fun FileName.play() {
         javafx.embed.swing.JFXPanel()
     }
     val resource = javaClass.classLoader.getResource(fileName)
+    if (resource == null) {
+        println("!! Error playing: $fileName")
+        return
+    }
     val media = Media(resource.toURI().toString())
-    mediaPlayer = MediaPlayer(media).apply {
+    MediaPlayer(media).apply {
         volume = VOLUME
+        onEndOfMedia = Runnable {
+            dispose()
+            players.remove(this)
+        }
+        // Keep a strong reference to players until they finish.
+        // Prevents sounds stopping early due to garbage collection.
+        players += this
         play()
     }
 }
