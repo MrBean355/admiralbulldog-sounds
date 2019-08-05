db6fc9c9251d447b12e50f1f85061282cbab4c59
Mike Johnston <mrbean355@gmail.com>
2019-08-05 17:50:45 +0200

    Fix match start sound byte logic

--- src/main/kotlin/com/github/mrbean355/dota2/integration/bytes/OnMatchStart.kt
+++ src/main/kotlin/com/github/mrbean355/dota2/integration/bytes/OnMatchStart.kt
@@ -2,9 +2,15 @@ package com.github.mrbean355.dota2.integration.bytes
 
 import com.github.mrbean355.dota2.integration.GameState
 
+/** Plays a sound just before the clock hits 0. */
 class OnMatchStart : SoundByte {
+    private var played = false
 
     override fun shouldPlay(previous: GameState, current: GameState): Boolean {
-        return previous.map?.matchid == null && current.map?.matchid != null
+        if (!played && current.map!!.clock_time >= -5) {
+            played = true
+            return true
+        }
+        return false
     }
 }
