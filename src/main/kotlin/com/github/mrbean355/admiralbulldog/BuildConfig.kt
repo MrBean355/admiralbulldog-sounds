8c1f08001e0a5dd1ddb10fb12aa7f8770888e02a
Mike Johnston <mrbean355@gmail.com>
2019-09-03 22:12:10 +0200

    Remove SNAPSHOT for release

--- src/main/kotlin/com/github/mrbean355/admiralbulldog/BuildConfig.kt
+++ src/main/kotlin/com/github/mrbean355/admiralbulldog/BuildConfig.kt
@@ -1,4 +1,4 @@
 package com.github.mrbean355.admiralbulldog
 
 @javax.annotation.processing.Generated("generateBuildConfig")
-const val APP_VERSION = "1.3.0-SNAPSHOT"
+const val APP_VERSION = "1.3.0"
