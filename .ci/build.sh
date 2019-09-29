45835f87cdbd076554688e8b595221f354c5d8ef
Mike Johnston <mrbean355@gmail.com>
2019-09-29 21:32:46 +0200

    Update CI scripts

--- .ci/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ "$TRAVIS_BRANCH" = "develop" ] && [ "$TRAVIS_PULL_REQUEST" != "true" ]; then
-    ./gradlew --no-daemon clean downloadSoundFiles build
-else
-    ./gradlew --no-daemon clean build
-fi
