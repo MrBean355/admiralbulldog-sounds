c61ecbaaa34376bdaa0c4ea87e4b5303dcc9874d
Mike Johnston <mrbean355@gmail.com>
2019-10-03 18:34:55 +0200

    Use coroutines

--- src/main/kotlin/com/github/mrbean355/admiralbulldog/assets/PlaySounds.kt
+++ src/main/kotlin/com/github/mrbean355/admiralbulldog/assets/PlaySounds.kt
@@ -1,7 +1,9 @@
 package com.github.mrbean355.admiralbulldog.assets
 
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.withContext
 import okhttp3.ResponseBody
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
@@ -14,28 +16,30 @@ private const val HOST_NUULS = "https://i.nuuls.com/"
 object PlaySounds {
 
     /** Scrape the PlaySounds web page and collect a list of sound file names and URLs. */
-    fun listRemoteFiles(): List<RemoteSoundFile> {
-        val response = createChatBotService().getHtml().execute()
+    suspend fun listRemoteFiles(): List<RemoteSoundFile> {
+        val response = createChatBotService().getHtml()
         val responseBody = response.body()
         if (!response.isSuccessful || responseBody == null) {
             throw RuntimeException("Unable to download HTML, code=${response.code()}, body length=${responseBody?.contentLength()}")
         }
-        val html = String(responseBody.bytes())
-        val blocks = html.split("<tr>")
-                .drop(2)
+        return withContext(IO) {
+            val html = String(responseBody.bytes())
+            val blocks = html.split("<tr>")
+                    .drop(2)
 
-        return blocks.map { block ->
-            val friendlyName = block.split("<td>")[1].split("</td>").first()
-            val url = block.split("data-link=\"")[1].split("\"").first()
-            val fileExtension = url.substringAfterLast('.', missingDelimiterValue = "")
-            val fileName = "$friendlyName.$fileExtension"
-            RemoteSoundFile(fileName, url)
+            blocks.map { block ->
+                val friendlyName = block.split("<td>")[1].split("</td>").first()
+                val url = block.split("data-link=\"")[1].split("\"").first()
+                val fileExtension = url.substringAfterLast('.', missingDelimiterValue = "")
+                val fileName = "$friendlyName.$fileExtension"
+                RemoteSoundFile(fileName, url)
+            }
         }
     }
 
     /** Download the given sound to the given destination. */
-    fun downloadFile(file: RemoteSoundFile) {
-        val response = createNuulsService().get(file.url.removePrefix(HOST_NUULS)).execute()
+    suspend fun downloadFile(file: RemoteSoundFile) {
+        val response = createNuulsService().get(file.url.removePrefix(HOST_NUULS))
         val responseBody = response.body()
         if (!response.isSuccessful || responseBody == null) {
             throw RuntimeException("Unable to download $file, code=${response.code()}, body length=${responseBody?.contentLength()}")
@@ -43,15 +47,17 @@ object PlaySounds {
         val stream = responseBody.byteStream()
         val output = FileOutputStream("$SOUNDS_PATH/${file.fileName}")
         val buffer = ByteArray(4096)
-        while (true) {
-            val read = stream.read(buffer)
-            if (read == -1) {
-                break
+        withContext(IO) {
+            while (true) {
+                val read = stream.read(buffer)
+                if (read == -1) {
+                    break
+                }
+                output.write(buffer, 0, read)
             }
-            output.write(buffer, 0, read)
+            output.close()
+            stream.close()
         }
-        output.close()
-        stream.close()
     }
 
     private fun createChatBotService(): PlaySoundsService {
@@ -78,12 +84,12 @@ object PlaySounds {
 
     private interface PlaySoundsService {
         @GET("playsounds")
-        fun getHtml(): Call<ResponseBody>
+        suspend fun getHtml(): Response<ResponseBody>
     }
 
     private interface NuulsService {
         @GET("{name}")
-        fun get(@retrofit2.http.Path("name") name: String): Call<ResponseBody>
+        suspend fun get(@retrofit2.http.Path("name") name: String): Response<ResponseBody>
     }
 }
 
