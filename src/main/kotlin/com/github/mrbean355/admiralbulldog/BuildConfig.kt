bdc1c2018a41fe7ee2500c79dfe4e1c37ba64f82
Mike Johnston <mrbean355@gmail.com>
2019-10-12 11:01:57 +0200

    Prepare for release

--- src/main/kotlin/com/github/mrbean355/admiralbulldog/BuildConfig.kt
+++ src/main/kotlin/com/github/mrbean355/admiralbulldog/BuildConfig.kt
@@ -1,4 +1,4 @@
 package com.github.mrbean355.admiralbulldog
 
 @javax.annotation.processing.Generated("generateBuildConfig")
-const val APP_VERSION = "1.6.0-SNAPSHOT"
+const val APP_VERSION = "1.6.0"
