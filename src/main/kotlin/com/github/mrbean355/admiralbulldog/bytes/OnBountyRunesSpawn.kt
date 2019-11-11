e55d69c80368e2baacdaf53cd5e91d8ec1b1a740
Mike Johnston <mrbean355@gmail.com>
2019-11-11 18:51:39 +0200

    Play the bounty runes sound before the initial runes

--- src/main/kotlin/com/github/mrbean355/admiralbulldog/bytes/OnBountyRunesSpawn.kt
+++ src/main/kotlin/com/github/mrbean355/admiralbulldog/bytes/OnBountyRunesSpawn.kt
@@ -1,38 +1,41 @@
 package com.github.mrbean355.admiralbulldog.bytes
 
 import com.github.mrbean355.admiralbulldog.game.GameState
-import java.util.concurrent.TimeUnit
+import com.github.mrbean355.admiralbulldog.game.MatchState
 import kotlin.math.ceil
 
+/** How often (in seconds) the runes spawn. */
+private const val HOW_OFTEN = 5 * 60L
+/** Play a sound this many seconds before the bounty runes spawn. */
+private const val WARNING_PERIOD = 15L
+
+/** Plays a sound shortly before the bounty runes spawn. */
 class OnBountyRunesSpawn : SoundByte {
-    private val howOften = TimeUnit.MINUTES.toSeconds(5)
-    private val warningPeriod = TimeUnit.SECONDS.toSeconds(15)
     private var nextPlayTime = UNINITIALISED
 
     override fun shouldPlay(previous: GameState, current: GameState): Boolean {
-        val currentTime = current.map!!.clock_time
-        if (currentTime <= 0) {
+        val gameState = current.map!!.game_state
+        if (gameState != MatchState.DOTA_GAMERULES_STATE_PRE_GAME && gameState != MatchState.DOTA_GAMERULES_STATE_GAME_IN_PROGRESS) {
+            // Don't play during the picking phase.
             return false
         }
+        val currentTime = current.map.clock_time
         if (nextPlayTime == UNINITIALISED) {
-            nextPlayTime = findIteration(currentTime)
+            nextPlayTime = findNextPlayTime(currentTime)
         }
         if (currentTime >= nextPlayTime) {
             val diff = currentTime - nextPlayTime
-            nextPlayTime = findIteration(currentTime + 10)
-            if (diff <= warningPeriod) {
+            nextPlayTime = findNextPlayTime(currentTime + 10)
+            if (diff <= WARNING_PERIOD) {
                 return true
             }
         }
         return false
     }
 
-    private fun findIteration(clockTime: Long): Long {
-        val iteration = ceil((clockTime + warningPeriod) / howOften.toFloat()).toInt()
-        val nextPlayTime = iteration * howOften - warningPeriod
-        if (nextPlayTime <= -warningPeriod) {
-            return -warningPeriod
-        }
-        return nextPlayTime
+    private fun findNextPlayTime(clockTime: Long): Long {
+        val iteration = ceil((clockTime + WARNING_PERIOD) / HOW_OFTEN.toFloat()).toInt()
+        val nextPlayTime = iteration * HOW_OFTEN - WARNING_PERIOD
+        return nextPlayTime.coerceAtLeast(-WARNING_PERIOD)
     }
 }
