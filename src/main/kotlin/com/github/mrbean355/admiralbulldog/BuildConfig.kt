beb55acf21d1e82a5c926c0e09195c0b300a93f3
Mike Johnston <MrBean355@users.noreply.github.com>
2020-03-09 21:04:15 +0200

    Bump version after release

--- src/main/kotlin/com/github/mrbean355/admiralbulldog/BuildConfig.kt
+++ src/main/kotlin/com/github/mrbean355/admiralbulldog/BuildConfig.kt
@@ -1,4 +1,4 @@
 package com.github.mrbean355.admiralbulldog
 
 @javax.annotation.processing.Generated("generateBuildConfig")
-val APP_VERSION = com.vdurmont.semver4j.Semver("1.9.1")
+val APP_VERSION = com.vdurmont.semver4j.Semver("1.10.0-SNAPSHOT")
