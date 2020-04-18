dc238bcadbb7c4912764e73634e948ea0bb3366d
Mike Johnston <mrbean355@gmail.com>
2020-04-18 15:44:41 +0200

    Improve update error handling
    When a FileNotFoundException happens, it can be caused by Dota being open.
    Show a specific error dialog which asks the user to close Dota.

--- src/main/kotlin/com/github/mrbean355/admiralbulldog/DownloadUpdateViewModel.kt
+++ src/main/kotlin/com/github/mrbean355/admiralbulldog/DownloadUpdateViewModel.kt
@@ -3,6 +3,7 @@ package com.github.mrbean355.admiralbulldog
 import com.github.mrbean355.admiralbulldog.arch.AssetInfo
 import com.github.mrbean355.admiralbulldog.arch.GitHubRepository
 import com.github.mrbean355.admiralbulldog.ui.format
+import com.github.mrbean355.admiralbulldog.ui.getString
 import com.github.mrbean355.admiralbulldog.ui.streamToFile
 import javafx.beans.property.SimpleBooleanProperty
 import javafx.beans.property.SimpleDoubleProperty
@@ -15,6 +16,7 @@ import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.File
+import java.io.FileNotFoundException
 
 class DownloadUpdateViewModel(
         private val assetInfo: AssetInfo,
@@ -24,40 +26,56 @@ class DownloadUpdateViewModel(
     private val coroutineScope = CoroutineScope(Default + Job())
     private val gitHubRepository = GitHubRepository()
 
-    val header = SimpleStringProperty("Downloading update...")
+    val header = SimpleStringProperty()
     val progress = SimpleDoubleProperty()
     val isComplete = SimpleBooleanProperty(false)
+    val error = SimpleStringProperty(null)
 
     fun init() {
+        header.set(getString("msg_downloading"))
         coroutineScope.launch {
             val resource = gitHubRepository.downloadAsset(assetInfo)
             val body = resource.body
             if (!resource.isSuccessful() || body == null) {
+                withContext(Main) {
+                    error.value = getString("msg_update_failed_unknown")
+                }
                 return@launch
             }
             val totalBytes = body.contentLength().toDouble()
             val totalMegabytes = totalBytes / 1024.0 / 1024
             val formatted = totalMegabytes.format(decimalPlaces = 2)
             withContext(Main) {
-                header.set("Downloading update ($formatted MB)...")
+                header.set(getString("msg_downloading_with_size", "$formatted MB"))
             }
             val directory = File(destination)
             if (!directory.exists()) {
                 directory.mkdirs()
             }
-            val file = File(directory, assetInfo.name)
-            body.byteStream().use {
-                it.streamToFile(file) { currentBytes ->
-                    withContext(Main) {
-                        progress.set(currentBytes / totalBytes)
+            try {
+                val file = File(directory, assetInfo.name)
+                body.byteStream().use {
+                    it.streamToFile(file) { currentBytes ->
+                        withContext(Main) {
+                            progress.set(currentBytes / totalBytes)
+                        }
                     }
                 }
-            }
-            withContext(Main) {
-                isComplete.set(true)
-                onDownloadComplete()
+                withContext(Main) {
+                    isComplete.set(true)
+                    onDownloadComplete()
+                }
+            } catch (e: FileNotFoundException) {
+                withContext(Main) {
+                    error.value = getString("msg_update_failed_file_not_found")
+                }
             }
         }
+    }
+
+    fun onRetryClicked() {
+        error.value = null
+        init()
     }
 
     fun onClose() {
