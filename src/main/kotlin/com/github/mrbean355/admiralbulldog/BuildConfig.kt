226835a8ea704a9f5d16e00a932230822ac90391
Mike Johnston <mrbean355@gmail.com>
2020-06-20 18:25:50 +0200

    Bump version after release

--- src/main/kotlin/com/github/mrbean355/admiralbulldog/BuildConfig.kt
+++ src/main/kotlin/com/github/mrbean355/admiralbulldog/BuildConfig.kt
@@ -2,5 +2,5 @@ package com.github.mrbean355.admiralbulldog
 
 import com.vdurmont.semver4j.Semver
 
-val APP_VERSION: Semver = Semver("1.10.0")
+val APP_VERSION: Semver = Semver("1.11.0-SNAPSHOT")
 const val DISTRIBUTION: String = "win"
