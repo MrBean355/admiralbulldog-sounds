c660c943fef9f27aaa25c81aee9554005c34bfc8
Mike Johnston <mrbean355@gmail.com>
2020-09-29 22:06:04 +0200

    Add viewModelScope to AppViewModel
    This scope defaults to the main dispatcher, removing the need to switch
    contexts within view models.

--- src/main/kotlin/com/github/mrbean355/admiralbulldog/arch/AppViewModel.kt
+++ src/main/kotlin/com/github/mrbean355/admiralbulldog/arch/AppViewModel.kt
@@ -2,13 +2,15 @@ package com.github.mrbean355.admiralbulldog.arch
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
-import tornadofx.ViewModel
-import tornadofx.runLater
+import tornadofx.*
 
 abstract class AppViewModel : ViewModel() {
+    @Deprecated("Use viewModelScope", replaceWith = ReplaceWith("viewModelScope"))
     protected val coroutineScope = CoroutineScope(IO + SupervisorJob())
+    protected val viewModelScope = CoroutineScope(Main.immediate + SupervisorJob())
 
     init {
         runLater { onReady() }
@@ -18,5 +20,6 @@ abstract class AppViewModel : ViewModel() {
 
     open fun onUndock() {
         coroutineScope.cancel()
+        viewModelScope.cancel()
     }
 }
