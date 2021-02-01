2bb6523f4c06283dc1f8b694a24cb2984dc833a0
Mike Johnston <mrbean355@gmail.com>
2021-02-01 23:16:44 +0200

    Fix always checking for new sounds
    When there were no new sounds the last time the app opened, it will
    always check for new sounds despite the update setting.

--- src/main/kotlin/com/github/mrbean355/admiralbulldog/BuildConfig.kt
+++ src/main/kotlin/com/github/mrbean355/admiralbulldog/BuildConfig.kt
@@ -19,6 +19,6 @@ package com.github.mrbean355.admiralbulldog
 import com.vdurmont.semver4j.Semver
 import kotlin.String
 
-public val APP_VERSION: Semver = Semver("1.12.0")
+public val APP_VERSION: Semver = Semver("1.12.1")
 
 public const val DISTRIBUTION: String = "win"
