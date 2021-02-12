65c2aa8d7967e680b6553d301b93d19f93d720a8
Mike Johnston <mrbean355@gmail.com>
2021-02-12 23:06:21 +0200

    Redesign sound trigger list
    Remove settings cog button, the whole list item can now be clicked.
    Highlight items when they are hovered over.
    Add a help button which shows a brief explanation of sound triggers.

--- src/main/kotlin/com/github/mrbean355/admiralbulldog/sounds/ViewSoundTriggersScreen.kt
+++ src/main/kotlin/com/github/mrbean355/admiralbulldog/sounds/ViewSoundTriggersScreen.kt
@@ -16,50 +16,60 @@
 
 package com.github.mrbean355.admiralbulldog.sounds
 
+import com.github.mrbean355.admiralbulldog.common.HelpIcon
 import com.github.mrbean355.admiralbulldog.common.PADDING_MEDIUM
 import com.github.mrbean355.admiralbulldog.common.PADDING_SMALL
-import com.github.mrbean355.admiralbulldog.common.SettingsIcon
 import com.github.mrbean355.admiralbulldog.common.WINDOW_WIDTH
 import com.github.mrbean355.admiralbulldog.common.getString
-import com.github.mrbean355.admiralbulldog.sounds.manager.SoundManagerScreen
 import com.github.mrbean355.admiralbulldog.triggers.SOUND_TRIGGER_TYPES
+import javafx.geometry.Pos.CENTER_LEFT
 import javafx.scene.image.ImageView
-import javafx.scene.layout.ColumnConstraints
-import javafx.scene.layout.Priority.ALWAYS
 import tornadofx.Fragment
 import tornadofx.Scope
 import tornadofx.action
 import tornadofx.button
-import tornadofx.gridpane
+import tornadofx.fitToParentWidth
+import tornadofx.hbox
 import tornadofx.label
 import tornadofx.paddingAll
-import tornadofx.row
+import tornadofx.paddingHorizontal
+import tornadofx.paddingTop
+import tornadofx.paddingVertical
+import tornadofx.spacer
+import tornadofx.vbox
 import tornadofx.whenUndocked
 
 class ViewSoundTriggersScreen : Fragment(getString("title_toggle_sound_triggers")) {
     private val viewModel by inject<ViewSoundTriggersViewModel>(Scope())
 
-    override val root = gridpane {
-        hgap = PADDING_SMALL
-        vgap = PADDING_SMALL
-        paddingAll = PADDING_MEDIUM
+    override val root = vbox {
+        paddingTop = PADDING_SMALL
         prefWidth = WINDOW_WIDTH
-        columnConstraints.addAll(ColumnConstraints().apply {
-            hgrow = ALWAYS
-        })
         SOUND_TRIGGER_TYPES.forEach { type ->
-            row {
-                label(viewModel.textProperty(type)) {
-                    textFillProperty().bind(viewModel.textColourProperty(type))
+            label(viewModel.textProperty(type)) {
+                paddingVertical = PADDING_SMALL * 2
+                paddingHorizontal = PADDING_MEDIUM * 2
+                fitToParentWidth()
+                textFillProperty().bind(viewModel.textColourProperty(type))
+                setOnMouseEntered {
+                    background = viewModel.highlightedBackground
                 }
-                button(graphic = ImageView(SettingsIcon())) {
-                    action { viewModel.onConfigureClicked(type) }
+                setOnMouseExited {
+                    background = viewModel.normalBackground
+                }
+                setOnMouseClicked {
+                    viewModel.onConfigureClicked(type)
                 }
             }
         }
-        row {
+        hbox(alignment = CENTER_LEFT) {
+            paddingAll = PADDING_MEDIUM
             button(getString("btn_manage_sounds")) {
-                action { find<SoundManagerScreen>().openModal(resizable = false) }
+                action { viewModel.onManageSoundsClicked() }
+            }
+            spacer()
+            button(graphic = ImageView(HelpIcon())) {
+                action { viewModel.onHelpClicked() }
             }
         }
     }
