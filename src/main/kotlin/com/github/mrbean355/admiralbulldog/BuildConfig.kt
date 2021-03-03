20c071e0d42a90965c5dad423cd973baa36efb66
Mike Johnston <mrbean355@gmail.com>
2021-03-03 21:16:58 +0200

    Prepare for release

--- src/main/kotlin/com/github/mrbean355/admiralbulldog/BuildConfig.kt
+++ src/main/kotlin/com/github/mrbean355/admiralbulldog/BuildConfig.kt
@@ -18,6 +18,6 @@ package com.github.mrbean355.admiralbulldog
 
 import com.vdurmont.semver4j.Semver
 
-val APP_VERSION: Semver = Semver("1.13.0-SNAPSHOT")
+val APP_VERSION: Semver = Semver("1.13.0")
 
 const val DISTRIBUTION: String = "win"
