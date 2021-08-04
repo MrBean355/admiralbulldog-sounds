e1e27981e447b02123d0561eecc93254838a5bc9
Mike Johnston <mrbean355@gmail.com>
2021-08-04 18:12:53 +0200

    Update Compose Desktop version

--- compose/build.gradle.kts
+++ compose/build.gradle.kts
@@ -19,7 +19,7 @@ import org.sonarqube.gradle.SonarQubeTask
 plugins {
     kotlin("jvm")
     kotlin("plugin.serialization") version "1.5.21"
-    id("org.jetbrains.compose") version "1.0.0-alpha1-rc5"
+    id("org.jetbrains.compose") version "1.0.0-alpha2"
     id("org.sonarqube") version "3.3"
     jacoco
 }
