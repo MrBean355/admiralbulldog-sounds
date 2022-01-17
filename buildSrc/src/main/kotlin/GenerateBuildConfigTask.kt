920d4b989cf56414f1557088cdb0229da033f821
Mike Johnston <mrbean355@gmail.com>
2022-01-17 23:32:51 +0200

    Update copyright for 2022

--- buildSrc/src/main/kotlin/GenerateBuildConfigTask.kt
+++ buildSrc/src/main/kotlin/GenerateBuildConfigTask.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Michael Johnston
+ * Copyright 2022 Michael Johnston
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,7 +37,7 @@ open class GenerateBuildConfigTask : DefaultTask() {
         output.writeText(
             """
             /*
-             * Copyright 2021 Michael Johnston
+             * Copyright 2022 Michael Johnston
              *
              * Licensed under the Apache License, Version 2.0 (the "License");
              * you may not use this file except in compliance with the License.
