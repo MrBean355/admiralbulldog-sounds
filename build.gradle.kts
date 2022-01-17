daaa4ca828a59c765f889ff0ea14e223b7fc1668
Mike Johnston <mrbean355@gmail.com>
2022-01-17 23:27:50 +0200

    Upgrade various dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.31"
-    id("org.jetbrains.compose") version "1.0.0-beta5"
+    kotlin("jvm") version "1.6.10"
+    id("org.jetbrains.compose") version "1.0.1"
 }
 
 group = "com.github.mrbean355"
@@ -29,10 +29,10 @@ val currentPlatform = (findProperty("platform") as? String) ?: "win"
 
 dependencies {
     implementation(kotlin("reflect"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-javafx:1.5.2")
-    implementation("io.ktor:ktor-server-netty:1.6.5")
-    implementation("io.ktor:ktor-gson:1.6.5")
-    implementation("org.slf4j:slf4j-simple:1.7.32")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-javafx:1.6.0")
+    implementation("io.ktor:ktor-server-netty:1.6.7")
+    implementation("io.ktor:ktor-gson:1.6.7")
+    implementation("org.slf4j:slf4j-simple:1.7.33")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
