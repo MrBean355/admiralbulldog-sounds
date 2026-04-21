a10769f3a491274587f720c700ae122a0c186241
Mike Johnston <mrbean355@gmail.com>
2026-04-21 20:10:56 +0200

    Bump version to 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.github.mrbean355"
-version = "1.16.0-SNAPSHOT"
+version = "2.0.0-SNAPSHOT"
 
 repositories {
     google()
--- src/jvmMain/kotlin/com/github/mrbean355/admiralbulldog/BuildConfig.kt
+++ src/jvmMain/kotlin/com/github/mrbean355/admiralbulldog/BuildConfig.kt
@@ -2,6 +2,6 @@ package com.github.mrbean355.admiralbulldog
 
 import com.vdurmont.semver4j.Semver
 
-val APP_VERSION: Semver = Semver("1.16.0-SNAPSHOT")
+val APP_VERSION: Semver = Semver("2.0.0-SNAPSHOT")
 
 const val DISTRIBUTION: String = "win"
